1b8a1d3534a34059b5ae9b01e14bdb02f9f52f42
Kostadinos J Kyprianos <196420087+alexandros-thomson@users.noreply.github.com>
2025-10-28 04:52:24 -0400

    Update verification token in messenger function
    description extended

--- netlify/functions/messenger.js
+++ netlify/functions/messenger.js
@@ -1,16 +1,1 @@
-exports.handler = async (event) => {
-  const params = event.queryStringParameters;
-  if (
-    params['hub.mode'] === 'subscribe' &&
-    params['hub.verify_token'] === 'ZEUS_THUNDER_2025'
-  ) {
-    return {
-      statusCode: 200,
-      body: params['hub.challenge']
-    };
-  }
-  return {
-    statusCode: 403,
-    body: 'Forbidden'
-  };
-};
+params['hub.verify_token'] === 'zeus_secure_messenger_verify_token_2025'
